d680bff1e1e86ed63b29167523e32300fe9976df
Oriol Bover Vila <bover73@gmail.com>
2026-01-31 12:46:14 +0100

    Refactor Image Caption Implementation
    - Removed hardcoded caption strings from translation files for both Catalan and English.
    - Updated Image component to use HTML for captions, enhancing formatting options.
    - Improved overall consistency in caption presentation across different language versions.

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -3,7 +3,6 @@ export default {
   'index.section': 'HOME',
   'index.title': '100 iconotextual reflections on visual communication and photography',
   'index.imageAlt': 'Light as a metaphor for language, photographic work by Toni Bover (1990-94)',
-  'index.caption': 'Toni Bover. Light as a metaphor for language. 1990-94.',
   'index.description':
     'This website constitutes an interactive iconotextual space that articulates 100 reflections on visual communication and photography, as well as on the relationships between art and science, humanism and technology, and between the external gaze and the internal gaze. The reflections can be read sequentially, by thematic areas or through keywords. In them I present perspectives on visual culture and image semiotics, influences received and autobiographical experiences.', // Navigation
   // Navigation
